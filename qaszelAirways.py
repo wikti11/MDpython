5a0ea7ba02be74f1d8f856f1ed11e7fbf08ebfc5
witki11 <wiktor.leszczynski.blue11@gmail.com>
2022-01-14 19:32:53 +0100

    Changed to incidence matrix

--- qaszelAirways.py
+++ qaszelAirways.py
@@ -6,38 +6,29 @@
 # Złożoność programu nie powinna być gorsza niż O(n^2+m), gdzie n to liczba miast, a m to liczba komend.
 # Program należy wykonać z użyceim macierzy.
 
-def countAmountOfConnections(matrix, value):
-    amountOfConnections = 0
-    for row in matrix:
-        for element in row:
-            if element == value:
-                amountOfConnections += 1
-    print(amountOfConnections)                                                      #złożoność tutaj to O(n^2)
-
-amountOfCities, amountOfCommands = input().split()                                  #zczytywanie ilości miast i komend
+amountOfCities, amountOfCommands = input().split()
 amountOfCities = int(amountOfCities)
 amountOfCommands = int(amountOfCommands)
-connectionsMatrix = []
-amountOfConnections = 0
-for i in range(amountOfCommands):                                                   #ma być tyle wejść z konsoli ile ilości komend, więc pojawia się for i złożoność zwiększa się do m
+connectionsMatrix = [[0 for x in range(amountOfCities)] for y in range(amountOfCities)]
+for i in range(amountOfCommands):
     choice, var1, *_ = input().split()
     choice = int(choice)
     var1 = int(var1)
     var2 = int(*_)
-    if choice == 1:                                                                 #dodanie połączenia, z tego co wiem to jest O(1)
-        if [var1, var2] not in connectionsMatrix:
-            connectionsMatrix.append([var1, var2])
-    elif choice == 2:                                                               #usunięcie połączenia, z tego co wiem to też jest O(1)
-        connectionToDelete = [var1, var2]
-        if connectionsMatrix and connectionToDelete in connectionsMatrix:
-            connectionsMatrix.remove(connectionToDelete)
-    elif choice == 3:                                                               #przeszukanie macierzy/tablicy, czyli O(n), więc złożoność tutaj to O(m*n)
-        connectionToLookup = [var1, var2]
-        if connectionToLookup in connectionsMatrix:
+    if choice == 1:
+        connectionsMatrix[var1][var2] = 1
+        connectionsMatrix[var2][var1] = 1
+    elif choice == 2:
+        connectionsMatrix[var1][var2] = 0
+        connectionsMatrix[var2][var1] = 0
+    elif choice == 3:
+        if connectionsMatrix[var1][var2] == 1:
             print("TAK")
         else:
             print("NIE")
     elif choice == 4:
-        countAmountOfConnections(connectionsMatrix, var1)                           #tutaj zliczam elementy w macierzy (wiersze 1-7), więc złożoność wzrasta do O(n^2*m)
-
-#Sprawdzarka pokazuje mi, że kod spełnia wymogi w 20%
+        amountOfConnections = 0
+        for j in range(amountOfCities):
+            if connectionsMatrix[var1][j] == 1:
+                amountOfConnections += 1
+        print(amountOfConnections)
